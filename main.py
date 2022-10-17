dd3d15809bac8261af5fd51ce98ab8b1f31208af
Luke Tanner <luke.tanner01@gmail.com>
2022-10-18 00:32:58 +0100

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+#character_name = "Bob"
+#character_age = "105"
+#print("There once was a man named " + character_name + ",")
+#print("he was " + character_age +  " years old.")
+#character_age = "75"
+#character_name = "Billy"
+#print("He really liked the name " + character_name+",")
+#print("but didn't like being " + character_age+".")
